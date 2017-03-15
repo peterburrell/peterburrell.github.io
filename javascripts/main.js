7f6a4c57df1cd5d8da3ae75d1bbdf918f79767e4
Peter Burrell <peterburrell@gmail.com>
2017-03-15 17:57:44 -0400

    cleanup

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,18 @@
-console.log('This would be the main JS file.');
+(function() {
+    "use strict";
+
+    var originalTitle = document.title;
+
+    function updateTitle() {
+        var alternateTitle = "I love 🍩s!",
+            currentTitle = document.title;
+
+        document.title = currentTitle === originalTitle ? 
+            alternateTitle : 
+            originalTitle;
+
+        window.setTimeout(updateTitle, 5000);
+    }
+
+    //window.setTimeout(updateTitle, 5000);
+})();
